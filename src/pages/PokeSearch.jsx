8a1f28bdc0d430e734080f442f5f2e372897c0b8
Anthony Buciu <richie@thegoatnest.com>
2023-01-09 12:07:51 +1000

    updated to show open and closed pokeball ui

--- src/pages/PokeSearch.jsx
+++ src/pages/PokeSearch.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from 'react';
 import './assets/pages-style.css';
 import './assets/GenOnePokemon.json';
 import { ReactComponent as PokeBall } from './assets/Pokeball-icon.svg';
-// import { ReactComponent as ClosedPokeBall } from './assets/Pokeball-icon-closed.svg';
+import { ReactComponent as ClosedPokeBall } from './assets/Pokeball-icon-closed.svg';
 
 export default function Content01() {
 
@@ -59,7 +59,7 @@ export default function Content01() {
 					Pokemon Search <br /> Search for your Pokemon!
 				</p>
 				<form onSubmit={onSubmit}>
-					<input type={'search'} ref={inputRef} />
+					<input placeholder='Search for your Pokemon' type={'search'} ref={inputRef} />
 					<button className="submit-button" type="submit">
 						Add
 					</button>
@@ -71,7 +71,7 @@ export default function Content01() {
 					<p style={{ fontWeight: 'bold' }}>Your Pokemon!</p>
 				</div>
 				<div className="Pokemon-slot" id="Pokemon01">
-					<PokeBall />
+					<ClosedPokeBall />
 					{Pokemon01.map((Pokemon01) => (
 						<div>{Pokemon01 || 'Pokemon slot 1'}</div>
 					))}
@@ -83,25 +83,25 @@ export default function Content01() {
 					))}
 				</div>
 				<div className="Pokemon-slot" id="Pokemon03">
-					<PokeBall />
+					<ClosedPokeBall />
 					{Pokemon03.map((Pokemon03) => (
 						<div>{Pokemon03 || 'Pokemon slot 3'}</div>
 					))}
 				</div>
 				<div className="Pokemon-slot" id="Pokemon04">
-					<PokeBall />
+					<ClosedPokeBall />
 					{Pokemon04.map((Pokemon04) => (
 						<div>{Pokemon04 || 'Pokemon slot 4'}</div>
 					))}
 				</div>
 				<div className="Pokemon-slot" id="Pokemon05">
-					<PokeBall />
+					<ClosedPokeBall />
 					{Pokemon05.map((Pokemon05) => (
 						<div>{Pokemon05 || 'Pokemon slot 5'}</div>
 					))}
 				</div>
 				<div className="Pokemon-slot" id="Pokemon06">
-					<PokeBall />
+					<ClosedPokeBall />
 					{Pokemon06.map((Pokemon06) => (
 						<div>{Pokemon06 || 'Pokemon slot 6'}</div>
 					))}
