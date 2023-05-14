b7b91bab63de21a6a77e0fedd70fa4c37185df51
Anthony Buciu <richie@thegoatnest.com>
2023-05-14 21:59:53 +1000

    make pokeslots component

--- src/pages/PokeSearch.jsx
+++ src/pages/PokeSearch.jsx
@@ -9,19 +9,19 @@ export default function Content01() {
 				<h1 className="title">
 					<strong>Your Pokemon!</strong>
 				</h1>
-				<div class="pokemonParty-Container">
-					<div class="partyGridZone1"><Pokeslot n="01" /></div>
-					<div class="partyGridZone2"><Pokeslot n="02" /></div>
-					<div class="partyGridZone3"><Pokeslot n="03" /></div>
-					<div class="partyGridZone4"></div>
-					<div class="partyGridZone5"></div>
-					<div class="partyGridZone6"></div>
-					<div class="partyGridZone7"><Pokeslot n="04" /></div>
-					<div class="partyGridZone8"><Pokeslot n="05" /></div>
-					<div class="partyGridZone9"><Pokeslot n="06" /></div>
-					<div class="partyGridZone10"></div>
-					<div class="partyGridZone11"></div>
-					<div class="partyGridZone12"></div>
+				<div className="pokemonParty-Container">
+					<div className="partyGridZone1"><Pokeslot n="01" /></div>
+					<div className="partyGridZone2"><Pokeslot n="02" /></div>
+					<div className="partyGridZone3"><Pokeslot n="03" /></div>
+					<div className="partyGridZone4"></div>
+					<div className="partyGridZone5"></div>
+					<div className="partyGridZone6"></div>
+					<div className="partyGridZone7"><Pokeslot n="04" /></div>
+					<div className="partyGridZone8"><Pokeslot n="05" /></div>
+					<div className="partyGridZone9"><Pokeslot n="06" /></div>
+					<div className="partyGridZone10"></div>
+					<div className="partyGridZone11"></div>
+					<div className="partyGridZone12"></div>
 				</div>
 			</section>
 		</div>
--- src/pages/assets/components/Pokeslot.jsx
+++ src/pages/assets/components/Pokeslot.jsx
@@ -7,7 +7,8 @@ import { ReactComponent as PokeBall } from '../Images/Pokeball-icon.svg';
 
 export default function Pokeslot(props) {
 	//#region Variables
-	const [setPokemon] = useState(['']);
+	let pokemon = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran", "Nidorina", "Nidoqueen", "Nidoran", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch'd", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew"];
+	const [setPokemon, setPokemonValue] = useState(['']);
 	const [slotActive, setActiveSlot] = useState('false');
 	const inputRef = useRef();
 	const pn = 'pokeSlot' + props.n.toString();
@@ -33,35 +34,36 @@ export default function Pokeslot(props) {
 	function onSubmit(e) {
 		e.preventDefault();
 		const value = inputRef.current.value;
-		if (value === '') return;
+		if (value === '') {return}
 		if (
 			document.getElementById(`input-${pn}`).getAttribute('class') ===
 			`${pn} active`
-		) {
-			setPokemon((prev) => {
+		){
+			setPokemonValue((prev) => {
 				return [value];
 			});
 		}
-		ActiveCheck(); //this makes all slots in-active after submission
 		inputRef.current.value = ''; //makes sure the field is empty after submitting
 	}
-
 	//#endregion
 
+//#region Pokemon Slot fields
 	return (
-		<div className={`container_${pn}`}>
+		<div className={`container_${pn} ${slotActive ? '' : 'active'}`}>
 			<form id={`form-${pn}`} onSubmit={onSubmit}>
 				<input
-					onSelect={ToggleSlot}
+					onFocus={ToggleSlot}
+					onBlur={ToggleSlot}
 					placeholder="Search your Pokemon"
 					id={`input-${pn}`}
 					type={'search'}
 					ref={inputRef}
-					autocomplete="off"
-					className={`${pn} ${slotActive ? '' : 'active'}`}
+					autoComplete="off"
+					className={`${pn}`}
 				/>
 			</form>
 			<PokeBall />
 		</div>
 	);
+//#endregion
 }
